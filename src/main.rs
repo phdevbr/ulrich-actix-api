37133385125ea4df080a6c7ddbfbabe863094e09
phdevbr <pedroheringer2020@gmail.com>
2023-03-19 14:07:33 +0000

    First implementation of API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
+use chrono::Local;
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let now = Local::now();
+    println!("{:#?} - API sucessfully started at 0.0.0.0:80", now);
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
 }
